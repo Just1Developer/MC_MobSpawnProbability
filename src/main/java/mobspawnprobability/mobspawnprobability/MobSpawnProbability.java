f7d87235d5b4fe043c251fd99a8a2ce5bedb8065
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2023-06-01 22:32:44 +0200

    Created Plugin and Debugged
    For some reason VCS in my IDE pushes the changes into the void so I had to kinda upload them manually... enjoy!

---
+++ src/main/java/mobspawnprobability/mobspawnprobability/MobSpawnProbability.java
@@ -0,0 +1,35 @@
+package mobspawnprobability.mobspawnprobability;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobSpawnProbability extends JavaPlugin implements Listener {
+    
+    public static MobSpawnProbability Instance;
+    
+    @Override
+    public void onEnable() {
+        Instance = this;
+        // Plugin startup logic
+        Config.loadProbabilites();
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+    
+    @EventHandler
+    public void onSpawn(EntitySpawnEvent e)
+    {
+        if(!Config.probabilities.containsKey(e.getEntityType())) return;
+        double worldSpawnRate;
+        String worldName = e.getLocation().getWorld().getName();
+        if(worldName.endsWith("_nether")) worldSpawnRate = Config.Nether;
+        else if(worldName.endsWith("_the_end")) worldSpawnRate = Config.End;
+        else worldSpawnRate = Config.Overworld;
+        
+        double Probability = worldSpawnRate * Config.probabilities.get(e.getEntityType());
+        // Deny cancel if the probability says so
+        if(Math.random() > Probability) e.setCancelled(true);
+    }
+}
